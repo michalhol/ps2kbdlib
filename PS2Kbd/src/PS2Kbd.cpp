1470f8051abdd4bb6da5f0f0fec5487b04ec6821
Petr Kubica <cubicap@email.cz>
2023-11-12 13:49:41 +0100

    possibly fixes synchronization error in #8

--- PS2Kbd/src/PS2Kbd.cpp
+++ PS2Kbd/src/PS2Kbd.cpp
@@ -120,7 +120,7 @@ void PS2Kbd::setLeds(uint8_t d) {
 }
 
 void ICACHE_RAM_ATTR PS2Kbd::interruptHandler() {
-    if(dirOUT)return;
+    if(dirOUT || digitalRead(clockPin)) return;
     shift>>=1;
     shift|=(digitalRead(dataPin)<<10);
     if(++rc==11){
